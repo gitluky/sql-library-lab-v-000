f19499885fefc1e5402e90021785d416fe119950
Jonathan Luk <jluk87@gmail.com>
2018-07-27 02:48:35 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,2 @@
-INSERT INTO series ()
+INSERT INTO series (title, author, sub_genre) VALUES ("Harry Potter", 1, "Magic");
+INSERT INTO series (title, author, sub_genre) VALUES ("Game of Thrones", 2, "Medieval");
