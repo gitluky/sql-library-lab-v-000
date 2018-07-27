0091b931c64a397ef3036d944a97ccf2836f51e0
Jonathan Luk <jluk87@gmail.com>
2018-07-27 02:39:08 +0000

    automatically backed up by learn

---
+++ lib/schema.sql
@@ -0,0 +1,32 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author INTEGER,
+  sub_genre INTEGER
+);
+
+CREATE TABLE sub_genre (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series INTEGER
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species INTEGER,
+  author INTEGER,
+  series INTEGER
+);
